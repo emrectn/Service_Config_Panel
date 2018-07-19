8dbef568ab237bd61625b499a9648ed883199340
Emre Çetin <emrec.tin@gmail.com>
2018-07-19 09:33:44 +0300

    Java Stream Tutorial in /test/StreamTutorial.Java

--- src/main/java/com/example/demo/model/PermType.java
+++ src/main/java/com/example/demo/model/PermType.java
@@ -18,22 +18,22 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 @Entity
-@Table(name="permtypes")
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "permtypes")
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 public class PermType implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
-	@Column(name = "permname", unique=true)
+	@Column(name = "permname", unique = true)
 	private String permname;
-	
+
 	@OneToMany(mappedBy = "permtype", cascade = CascadeType.ALL, orphanRemoval = true)
 	@JsonSerialize(using = CustomUserSerializer.class)
 	private List<User> user = new ArrayList<>();
-	
+
 	public PermType() {
 		super();
 	}
@@ -41,6 +41,13 @@ public class PermType implements Serializable {
 	public PermType(String permname) {
 		super();
 		this.permname = permname;
+	}
+
+	private PermType(Integer id, String permname, List<User> user) {
+		super();
+		this.id = id;
+		this.permname = permname;
+		this.user = user;
 	}
 
 	public Integer getId() {
@@ -58,20 +65,44 @@ public class PermType implements Serializable {
 	public void setPermname(String permname) {
 		this.permname = permname;
 	}
-	
+
 	public List<User> getUser() {
 		return user;
 	}
-	
+
 	public void setUser(List<User> user) {
 		this.user = user;
+	}
+
+	public static class Builder {
+		private Integer id;
+		private String permname;
+		private List<User> user = new ArrayList<>();
+
+		public Builder withId(Integer id) {
+			this.id = id;
+			return this;
+		}
+
+		public Builder withPermName(String permname) {
+			this.permname = permname;
+			return this;
+		}
+
+		public Builder withUsers(List<User> user) {
+			this.user = user;
+			return this;
+		}
+
+		public PermType createPerm() {
+			return new PermType(id, permname, user);
+		}
+
 	}
 
 	@Override
 	public String toString() {
 		return "PermType [id=" + id + ", permname=" + permname + "]";
 	}
-	
-	
 
 }
---
+++ src/test/java/com/example/demo/service/test/StreamTutorial.java
@@ -0,0 +1,111 @@
+package com.example.demo.service.test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.ListUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.example.demo.MainApplication;
+import com.example.demo.model.PermType;
+import com.example.demo.service.PermTypeService;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = MainApplication.class)
+public class StreamTutorial {
+
+	@Autowired
+	private PermTypeService permTypeService;
+
+	@Test
+	public void deneme() {
+
+		String a = null;
+		
+		List<PermType> permTypes = permTypeService.findAll();
+				
+		PermType permType = new PermType.Builder()
+				.withPermName("")
+				.withId(Integer.valueOf(1))
+				.withUsers(Collections.emptyList())
+				.createPerm();
+		
+		/**
+		 * emptyIfNull :: Eğer Liste null gelirse NullPointerExceptiondan kurtuluyoruz.
+		 * Objects::nonNull Eğer liste içindeki değer null ise NPE kurtuluyoruz.
+		 * StringUtils x.getPermname den gelicek null için NPE kurtuluyoruz.
+		 **/
+		
+		System.out.println("NPE kurtulmak için");
+		ListUtils.emptyIfNull(permTypes).stream().filter(Objects::nonNull).filter(x -> StringUtils.startsWith(x.getPermname(), "")).forEach(System.out::println);
+		assertTrue(CollectionUtils.isNotEmpty(permTypes));
+		
+		// P ile başlayanları sıralı şekilde gösterelim
+		System.out.println("-----------------------------");
+		System.out.println("Liste içerisinde m ile başlayanlar");
+		ListUtils.emptyIfNull(permTypes).stream().filter(Objects::nonNull).filter(x -> StringUtils.startsWith(x.getPermname().toLowerCase(), "m")).forEachOrdered(System.out::println);
+		
+		//Eşit Değerlerin elde edilmesi
+		System.out.println("-----------------------------");
+		System.out.println("Liste elemanı management'a eşit olan");
+		List<PermType> example = permTypes.stream().filter(degerler-> "management".equalsIgnoreCase(degerler.getPermname())).collect(Collectors.toList());
+		example.stream().forEach(System.out::println);
+		
+		//Eger içinde geçiyorsa
+		System.out.println("-----------------------------");
+		System.out.println("Liste elemanları içerisinde 'man' geçen");
+		List<PermType> example1 = permTypes.stream().filter(degerler-> degerler.getPermname().toLowerCase().contains("man".toLowerCase())).collect(Collectors.toList());
+		example1.stream().forEach(System.out::println);
+		
+		//Filter için fonksiyon olutşruma
+		System.out.println("-----------------------------");
+		System.out.println("Filter için fonksiyon olutşruma");
+		Stream.of("d2", "a2", "b1", "b3", "c")
+		.filter(s -> {
+			System.out.println("filter : " + s);
+			return true;
+		})
+		.forEach(s -> System.out.println("forEach : " + s));
+		
+		//Listedeki isimleri başka listeye taşıma
+
+		System.out.println("-----------------------------");
+		System.out.println("Listedeki isimleri başka listeye taşıma");
+		List<String> permNames = permTypes.stream().map(x -> x.getPermname()).collect(Collectors.toList());
+		System.out.println(permNames);
+		
+		// Listedeki id'leri toplama
+		System.out.println("-----------------------------");
+		System.out.println("Listedeki id'leri toplama");
+		System.out.println(permTypes.stream().mapToInt(PermType::getId).sum());
+		/*ForEach(PermType p : permTypes) gibi
+		 * mapToInt(PermType oldugunu biliyoruz nesne adı vermiyoruz. O sınıfın getId fonksiyonunu çağırıyoruz) Tek bir stream oluşturuyoruz.
+		 * */
+		
+		//FlatMap
+		System.out.println("-----------------------------");
+		System.out.println("Flat Map");
+		List<String> firstNames = Arrays.asList("Emre", "Erdem", "Reyyan", "Melih");
+		List<String> lastNames = Arrays.asList("Çetin", "Aydin","arik", "pektaş");
+		
+		List<String> names = Stream.of(firstNames, lastNames).flatMap(l -> l.stream()).collect(Collectors.toList());
+		names.forEach(System.out::println);
+		
+
+		
+		
+	}
+}
